c1c4673d0bee8e42a40b5ed28dc353a0b2afbc86
Guillaume Huysmans <ghuysmans99@gmail.com>
2016-02-21 14:33:39 +0100

    Made a trivial syntax checker

--- pascal/patoc.c
+++ pascal/patoc.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include "pascal.tab.h"
 
 int main(int argc, char *argv[]) {
-	printf("usage: %s source.pas\n", argv[0]);
-	return 1;
+	if (argc != 2) {
+		printf("usage: %s source.pas\n", argv[0]);
+		return 1;
+	}
+	yyparse();
+	return 0;
 }
